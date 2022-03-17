91957861ae784031f76b77112a57dcc252e2deac
Jason Bongiovanni <jpbongiovanni@gmail.com>
2022-03-17 14:50:45 -0400

    finished

--- src/main/java/com/bongo/employeemanager2/EmployeeResource.java
+++ src/main/java/com/bongo/employeemanager2/EmployeeResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +50,7 @@ public class EmployeeResource {
 		return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
 	}
 	
+	@Transactional
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
 		employeeService.deleteEmployee(id);
